ff79e8877662396ea96f7022bced119c87a485cd
dhruv716 <121818483+dhruv716@users.noreply.github.com>
2024-02-20 21:38:44 +0530

    Update langchain_fiass_falcon.py

--- Version2/FIASS_FALCON/langchain_fiass_falcon.py
+++ Version2/FIASS_FALCON/langchain_fiass_falcon.py
@@ -1,1 +1,45 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.prompts import PromptTemplate
+from langchain.indexes.vectorstore import VectorstoreIndexCreator
+from langchain import HuggingFaceHub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain.chains import RetrievalQA
+from langchain.schema import retriever
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+def create_vector_store(file_path):
+    
+    with open(file_path) as f:
+        state_of_the_union = f.read()
+    
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=350,chunk_overlap=20)
+    chunks = text_splitter.split_text(state_of_the_union)
+    
+    embeddings = HuggingFaceEmbeddings()
+    
+    vectorStore = FAISS.from_texts(chunks, embeddings)
+    
+    return vectorStore
+
+def get_result(query):
+    llm=HuggingFaceHub(huggingfacehub_api_token=os.environ['HUGGINGFACEHUB_API_TOKEN'], repo_id="tiiuae/falcon-7b-instruct", model_kwargs={"temperature":0.1 ,"max_length":512})
+    vector_store = create_vector_store("/Users/dhruvpai/Downloads/WhisperAI/Version2/knowledge.txt")
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vector_store.as_retriever())
+    result = chain.run(query)
+    
+    return result
+
+def main():
+    query = "What are the challenges of Decision Trees?"
+    result = get_result(query)
+    print(result)
+    
+main()
+    
+
 
